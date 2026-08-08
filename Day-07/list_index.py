be1d1ad9a59f6e65e75b42a612d18abf2e01fe52
Aliya Pathan <pathanaliya563@gmail.com>
2026-08-09 04:55:27 +0530

    Day 7: Python Lists

---
+++ Day-07/list_index.py
@@ -0,0 +1,8 @@
+fruits = ["Apple","Banana","Mango","Orango"]
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
+
+print(fruits[-1])
---
+++ Day-07/list_methods.py
@@ -0,0 +1,7 @@
+fruits = ["Apple","Banana","Mango"]
+
+fruits.append("Orange")
+print("After adding:", fruits)
+
+fruits.remove("Banana")
+print("After removing:", fruits)
