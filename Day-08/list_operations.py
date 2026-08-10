e4163a82a9eedbc6339318ad05874b881c56b96b
Aliya Pathan <pathanaliya563@gmail.com>
2026-08-10 05:46:03 +0530

    Day 8:List Operations

---
+++ Day-08/list_operations.py
@@ -0,0 +1,7 @@
+fruits = ["Apple", "Banana", "Mango"]
+
+print("Number of fruits:", len(fruits))
+
+fruits.insert(1, "Orange")
+
+print("After inserting:", fruits)
---
+++ Day-08/remove_items.py
@@ -0,0 +1,12 @@
+fruits = ["Apple", "Banana","Mango","Orange"]
+
+
+removed_fruit = fruits.pop(2)
+
+print("Removed:", removed_fruit)
+print("After pop:", fruits)
+
+
+del fruits[0]
+
+print("After del:", fruits)
