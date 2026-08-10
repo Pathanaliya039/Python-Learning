4270b532e861000b69ef8748b6ac964a97a71716
Aliya Pathan <pathanaliya563@gmail.com>
2026-08-11 04:54:17 +0530

    Day 9: Tuples and Sets

---
+++ Day-09/set_basic.py
@@ -0,0 +1,3 @@
+numbers ={1,2,3,2,4,3,5}
+
+print(numbers)
---
+++ Day-09/tuple_unpacking.py
@@ -0,0 +1,7 @@
+student = ("Aliya",19,"BCS")
+
+name, age, course = student
+
+print("Name:",name)
+print("Age:",age)
+print("Course:",course)
