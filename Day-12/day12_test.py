56f65f46091b7f4da04e763976bc3821798090e5
Aliya Pathan <pathanaliya563@gmail.com>
2026-08-14 05:13:27 +0530

    Day 12:Python Functions

---
+++ Day-12/day12_test.py
@@ -0,0 +1,24 @@
+#Task 1
+def greet():
+    print("Welcome to Python!")
+
+greet()
+
+
+# Task 2 
+def square(number):
+    result = number * number
+    print(result)
+
+square(5)
+
+
+
+# Task 3
+def multiply(a, b):
+    result = a * b
+    return result
+
+answer = multiply(10, 6)
+
+print("Result:", answer)
