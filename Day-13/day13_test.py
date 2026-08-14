2f14a83625dda7a4fa6ae6643d6d92cd582d0c16
Aliya Pathan <pathanaliya563@gmail.com>
2026-08-15 05:26:46 +0530

    Day 13: Python Function Parameters

---
+++ Day-13/day13_test.py
@@ -0,0 +1,22 @@
+#Task 1
+def book_info(title, author, year):
+    print("Title:", title)
+    print("Author:", author)
+    print("Year:", year)
+
+book_info("Python Basics", "Aliya", 2026)
+
+#Task 2
+def country(name="India"):
+    print("Country:", name)
+
+country()
+country("Japan")
+
+#Task 3
+def student_info(name, age, course):
+    print("Name:", name)
+    print("Age:", age)
+    print("Course:", course)
+
+student_info(name="Aliya", age=20, course="BCS")
---
+++ Day-13/function_default.py
@@ -0,0 +1,5 @@
+def greet(name="Aliya"):
+    print("Hello", name)
+
+greet()
+greet("Python")
