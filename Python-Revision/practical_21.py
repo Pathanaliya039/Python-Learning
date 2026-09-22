a0eef6d4dbdd3939272dcb581507ae0f6cac981e
Aliya Pathan <pathanaliya563@gmail.com>
2026-09-23 05:12:23 +0530

    Add Practical 21: Student Marks Analyzer

---
+++ Python-Revision/practical_21.py
@@ -0,0 +1,16 @@
+marks = [75, 82, 64, 91, 58]
+print("marks:", marks)
+
+len(marks)
+print("Length of marks:", len(marks))
+
+sum(marks)
+print("Sum of marks:", sum(marks))
+
+average = sum(marks) / len(marks)
+print("Average of marks:", average)
+
+max_mark = max(marks)
+min_mark = min(marks)
+print("Maximum mark:", max_mark)
+print("Minimum mark:", min_mark)
